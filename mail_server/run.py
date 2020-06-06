b670ec18d653f5ee3b04e764b35490f7ec6bb53d
sori9899 <hyun989969@gmail.com>
2020-06-06 17:20:15 +0900

    메일 전송부분

--- mail_server/run.py
+++ mail_server/run.py
@@ -1,1 +1,31 @@
-# 김성욱
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def sender(recidpients, sw_post, department_post):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('syi.superuser@gmail.com','hxuglvvfbtxjhdqr')
+    body=""
+
+    for item in range(0, len(recidpients)):
+        print('이메일 보내는중 :' + recidpients(item))
+        msg =MIMEMultipart('alternative')
+
+        msg['Subject']='SeaYourInfo 임시 비밀번호 발급'
+        msg['From'] = 'syi.superuser@gmail.com'
+        msg['To'] = recidpients(item)
+
+        msg.attach(MIMEText(body, 'html'))
+
+        server.send_message(msg)
+
+    print('이메일 보내기 완료')
+    server.quit()
+
+
+if __name__ == '__main__':
+    list_json = []
+    sw_post=[]
+    department_post=[]
+    sender(list_json, sw_post, department_post)
